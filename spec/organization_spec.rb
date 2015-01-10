717f5e47acc101a5df24d5cd0745f36a05f3caaf
Andrew Frank <andrew.frank@wiredwithpurpose.com>
2015-01-10 18:29:07 -0500

    Able to set the depth to 0, 1, or 2.

--- spec/organization_spec.rb
+++ spec/organization_spec.rb
@@ -5,9 +5,12 @@ describe Organization do
   Given(:subject) { described_class.new }
 
   describe '.depth' do
-    context 'can be set to 0' do
-      When(:result) { subject.depth = 0 }
-      Then { subject.depth == 0 }
+    valid_depths = [0, 1, 2]
+    valid_depths.each do |depth|
+      context "can be set to #{depth}" do
+        When(:result) { subject.depth = depth }
+        Then { subject.depth == depth }
+      end
     end
   end
 
