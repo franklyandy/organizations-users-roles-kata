e01ddc8b889c225d83bd5c690b9870e9224a3f4e
Andrew Frank <andrew.frank@wiredwithpurpose.com>
2015-01-11 08:29:09 -0500

    Depth cannot be a negative number.

--- lib/organization.rb
+++ lib/organization.rb
@@ -1,3 +1,8 @@
 class Organization
   attr_accessor :depth
+
+  def depth=(depth)
+    raise ArgumentError, "depth can only be 0, 1, or 2" if depth < 0
+    @depth = depth
+  end
 end
--- spec/organization_spec.rb
+++ spec/organization_spec.rb
@@ -1,17 +1,24 @@
 require 'organization';
 
 describe Organization do
-
   Given(:subject) { described_class.new }
 
   describe '.depth' do
+    When(:result) { subject.depth = depth_to_set }
+
     valid_depths = [0, 1, 2]
     valid_depths.each do |depth|
       context "can be set to #{depth}" do
-        When(:result) { subject.depth = depth }
+        Given(:depth_to_set) { depth }
         Then { subject.depth == depth }
       end
     end
+
+    context 'cannot be less than 0' do
+      Given(:depth_to_set) { -1 }
+      Then { result == Failure(ArgumentError, /depth can only be 0, 1, or 2/) }
+    end
+
   end
 
 end
